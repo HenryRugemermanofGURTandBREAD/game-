d6458c4b05693f6d4b8af78c41e5719c10e021f6
HenryRugemermanofGURTandBREAD <113529523+HenryRugemermanofGURTandBREAD@users.noreply.github.com>
2023-02-14 09:55:08 -0500

    Add files via upload

---
+++ pygame_henry/pygame_henry/click_game_finaldraft_HFR.py
@@ -0,0 +1,30 @@
+import time
+
+WIDTH = 500
+HEIGHT = 300
+
+urmum = Actor('character')
+urmum.pos = 100,50
+
+def draw():
+    screen.fill((0, 255, 0))
+    urmum.draw()
+
+def update():
+    urmum.left = urmum.left + 2
+    if urmum.left > WIDTH:
+        urmum.right = 0
+
+
+
+def on_mouse_down(pos):
+    if urmum.collidepoint(pos):
+        set_character_hurt()
+
+def set_character_normal():
+    urmum.image = 'character'
+
+def set_character_hurt():
+    urmum.image = 'character_clicked'
+    sounds.eep.play()
+    clock.schedule_unique(set_character_normal, 1.0)
